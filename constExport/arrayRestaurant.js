fff2327859936fbf53cf9409e70e584a251f11e4
Angel <araozangel842@gmail.com>
2021-10-26 22:24:03 +0300

    Array outside of component

---
+++ constExport/arrayRestaurant.js
@@ -0,0 +1,28 @@
+export const localRestaurants = [
+	{
+		name: 'Havana Bar',
+		image_url:
+			'https://res.cloudinary.com/dqaerysgb/image/upload/v1635267338/restaurat-bar_m41i8l.jpg',
+		categories: ['Cafe', 'Bar'],
+		price: '£',
+		reviews: 1254,
+		rating: 4.5,
+	},
+	{
+		name: 'Fintum Caffe',
+		image_url:
+			'https://res.cloudinary.com/dqaerysgb/image/upload/v1635267337/bar-kitchen_i9c4ns.jpg',
+		categories: ['Cafe', 'Bar'],
+		price: '£',
+		reviews: 2219,
+		rating: 3.9,
+	},
+	{
+		name: 'Indian Kitchen and Brothers',
+		image_url: 'https://res.cloudinary.com/dqaerysgb/image/upload/v1635267337/eco-bar_yz6nzt.jpg',
+		categories: ['Indian', 'Bar'],
+		price: '£',
+		reviews: 3452,
+		rating: 4.8,
+	},
+];
