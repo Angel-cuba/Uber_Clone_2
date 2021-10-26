8cf76ef0bbedee5ddd5f8f54864fbbe9b75ab411
Angel <araozangel842@gmail.com>
2021-10-26 22:24:36 +0300

    Fetching data from YELP API

--- screens/Home.js
+++ screens/Home.js
@@ -1,17 +1,48 @@
-import React from 'react';
-import { StyleSheet, View, SafeAreaView } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, View, SafeAreaView, ScrollView } from 'react-native';
 import HeaderTab from '../components/HeaderTab';
 import SearchBar from '../components/SearchBar';
 import Categories from '../components/Categories';
+import Restaurants from '../components/Restaurants';
+import { localRestaurants } from '../constExport/arrayRestaurant';
+
+const YELP_API_KEY =
+	'yyZKqfn-Uo90nJ9y5P3hJqFjuqlTdm7AHg8iP6sU_8tHjE4se_1EFloVoI_4I2ZP_7tyCm0bq8_g2b0nsltv_OhvkzFH7IyM8xl8KCH6JLD3mkkDKZOuUqkSckx4YXYx';
 
 const Home = () => {
+	const [restaurantData, setRestaurantData] = useState(localRestaurants);
+	console.log(restaurantData);
+
+	const getRestaurantFromYelp = () => {
+		const yelpUrl = 'https://api.yelp.com/v3/businesses/search?term=restaurant&location=Turku';
+
+		const apiOptions = {
+			headers: {
+				Authorization: `Bearer ${YELP_API_KEY}`,
+			},
+		};
+
+		return fetch(yelpUrl, apiOptions)
+			.then((response) => response.json())
+			.then((json) => setRestaurantData(json.businesses));
+	};
+
+	useEffect(() => {
+		getRestaurantFromYelp();
+	}, []);
+
 	return (
 		<SafeAreaView style={styles.container}>
 			<View style={styles.insideContainer}>
 				<HeaderTab />
 				<SearchBar />
 			</View>
-			<Categories />
+
+			<ScrollView showsVerticalScrollIndicator={false}>
+				<Categories />
+
+				<Restaurants restaurantData={restaurantData} />
+			</ScrollView>
 		</SafeAreaView>
 	);
 };
