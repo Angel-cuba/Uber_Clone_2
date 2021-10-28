b01fa9ff5b17780aeb0d367b31237301791a4125
Angel <araozangel842@gmail.com>
2021-10-28 18:42:07 +0300

    Fetching all info from parent component

--- components/restaurantDetails/About.js
+++ components/restaurantDetails/About.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Image } from 'react-native';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const yelpRestaurantInfo = {
-	name: 'Famous places around the world',
+	name: 'Famous places near you',
 	image: 'https://res.cloudinary.com/dqaerysgb/image/upload/v1635267337/bar-kitchen_i9c4ns.jpg',
 	reviews: '1499',
 	price: '££',
@@ -11,19 +12,22 @@ const yelpRestaurantInfo = {
 	categories: [{ title: 'Nice food' }, { title: 'Comfort food' }],
 };
 
-const { name, image, price, reviews, rating, categories } = yelpRestaurantInfo;
+// const { name, image, price, reviews, rating, categories } = yelpRestaurantInfo;
 
-const formattedCategories = categories.map((cat) => cat.title).join(' - ');
+const About = ({ route, navigation }) => {
+	const { name, image, price, reviews, rating, categories } = route.params;
+	const formattedCategories = categories.map((cat) => cat.title).join(' - ');
 
-const description = `${formattedCategories} ${
-	price ? ' - ' + price : ''
-} - 💳 - ${rating} 🎇 (${reviews}+)`;
-
-const About = () => {
+	const description = `${formattedCategories} ${
+		price ? ' - ' + price : ''
+	} - 💳 - ${rating} 🎇 (${reviews}+)`;
 	return (
 		<View>
 			{/* Restaurant Image */}
 			<RestaurantImage image={image} />
+			<TouchableOpacity style={{ marginLeft: 10 }} onPress={() => navigation.goBack()}>
+				<MaterialCommunityIcons name="arrow-left" size={25} color="silver" />
+			</TouchableOpacity>
 			{/* Restaurant Title */}
 			<RestaurantName name={name} />
 			{/* Restaurant description */}
@@ -33,7 +37,7 @@ const About = () => {
 };
 
 const RestaurantImage = (props) => (
-	<Image source={{ uri: props.image }} style={{ width: '100%', height: 180 }} />
+	<Image source={{ uri: props.image }} style={{ width: '100%', height: 200 }} />
 );
 
 const RestaurantName = (props) => (
@@ -42,13 +46,17 @@ const RestaurantName = (props) => (
 			fontSize: 30,
 			fontWeight: '700',
 			marginTop: 10,
+			marginHorizontal: 10,
+			textAlign: 'center',
 		}}
 	>
 		{props.name}
 	</Text>
 );
 const RestaurantDescription = (props) => (
-	<Text style={{ fontSize: 16, marginHorizontal: 15, fontWeight: '400' }}>{props.description}</Text>
+	<Text style={{ fontSize: 16, marginHorizontal: 15, fontWeight: '400', marginTop: 10 }}>
+		{props.description}
+	</Text>
 );
 export default About;
 
