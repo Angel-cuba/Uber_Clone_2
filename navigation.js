74ab61c72c6ca4f9808d79d9d40b1d2be34d0837
Angel <araozangel842@gmail.com>
2021-10-30 00:48:32 +0300

    Issue of currecy in Android is fixed

--- navigation.js
+++ navigation.js
@@ -1,9 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Platform } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import Home from './screens/Home';
 import RestaurantDetail from './screens/RestaurantDetail';
+
+import { Provider as ReduxProvider } from 'react-redux';
+import configureStore from './redux/store';
+
+if (Platform.OS === 'android') {
+	require('intl');
+	require('intl/locale-data/jsonp/fr-BE');
+	require('intl/locale-data/jsonp/nl-BE');
+	require('intl/locale-data/jsonp/it-IT');
+}
+
+const store = configureStore();
 
 const RootNavigation = () => {
 	const Stack = createNativeStackNavigator();
@@ -13,12 +25,14 @@ const RootNavigation = () => {
 	};
 
 	return (
-		<NavigationContainer>
-			<Stack.Navigator initialRouteName="Home" screenOptions={screenOptions}>
-				<Stack.Screen name="Home" component={Home} />
-				<Stack.Screen name="RestaurantDetail" component={RestaurantDetail} />
-			</Stack.Navigator>
-		</NavigationContainer>
+		<ReduxProvider store={store}>
+			<NavigationContainer>
+				<Stack.Navigator initialRouteName="Home" screenOptions={screenOptions}>
+					<Stack.Screen name="Home" component={Home} />
+					<Stack.Screen name="RestaurantDetail" component={RestaurantDetail} />
+				</Stack.Navigator>
+			</NavigationContainer>
+		</ReduxProvider>
 	);
 };
 
