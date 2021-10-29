7a47efd3768072bbc637f25daac778698eaebf78
Angel <araozangel842@gmail.com>
2021-10-30 00:47:42 +0300

    Working with reducer

--- redux/reducers/cartReducer.js
+++ redux/reducers/cartReducer.js
@@ -2,17 +2,33 @@ let defaultState = {
 	selectedItems: { items: [], restaurantName: '' },
 };
 
-let catReducer = (state = defaultState, action) => {
+let cartReducer = (state = defaultState, action) => {
 	switch (action.type) {
 		case 'ADD_TO_CART': {
 			let newState = { ...state };
-			newState.selectedItems = {
-				items: [...newState.selectedItems.items, action.payload],
-				restaurantName: action.payload.restaurantName,
-			};
+			if (action.payload.checkboxValue) {
+				console.log('ADD_TO_CART');
+
+				newState.selectedItems = {
+					items: [...newState.selectedItems.items, action.payload],
+					restaurantName: action.payload.restaurantName,
+				};
+			} else {
+				newState.selectedItems = {
+					items: [
+						...newState.selectedItems.items.filter((item) => item.title !== action.payload.title),
+					],
+					restaurantName: action.payload.restaurantName,
+				};
+				console.log('remove from state', newState);
+			}
 			console.log('new state', newState);
+			return newState;
 		}
+
 		default:
 			return state;
 	}
 };
+
+export default cartReducer;
