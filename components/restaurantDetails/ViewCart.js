d571caf042d9531ddf045b61d0386d0c0e3279eb
Angel <araozangel842@gmail.com>
2021-10-31 17:02:23 +0200

    Showing Modal and fetching data to OrderItem

--- components/restaurantDetails/ViewCart.js
+++ components/restaurantDetails/ViewCart.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import { Platform, StyleSheet, Text, TouchableOpacity, View, Modal } from 'react-native';
 import { useSelector } from 'react-redux';
+import OrderItem from './OrderItem';
 
 const ViewCart = () => {
 	const [modalVisible, setModalVisible] = useState(false);
-	const items = useSelector((state) => state.cartReducer.selectedItems.items);
+	const { items, restaurantName } = useSelector((state) => state.cartReducer.selectedItems);
 	// console.log('items---->', items[0].price);
+	//console.log(restaurantName);
 	const total = items
 		.map((item) => Number(item.price.replace('£', '')))
 		.reduce((prev, curr) => prev + curr, 0);
@@ -17,13 +19,20 @@ const ViewCart = () => {
 
 	const checkCurrentModal = () => {
 		return (
-			<View style={styles.modalContainer}>
-				<View style={styles.modal}>
-					<TouchableOpacity onPress={() => setModalVisible(false)}>
-						<Text style={{ color: 'white' }}>Checkout</Text>
-					</TouchableOpacity>
+			<>
+				<View style={styles.priceModalContainer}>
+					<View style={styles.modalCheckOutContainer}>
+						<Text style={styles.restaurantName}>{restaurantName}</Text>
+						{items.map((item, index) => (
+							<OrderItem key={index} item={item} />
+						))}
+						<View style={styles.subTotalContainer}>
+							<Text style={styles.subtotalText}>SubTotal</Text>
+							<Text>{totalUSD}</Text>
+						</View>
+					</View>
 				</View>
-			</View>
+			</>
 		);
 	};
 
@@ -74,9 +83,8 @@ const styles = StyleSheet.create({
 		flexDirection: 'row',
 		position: 'absolute',
 		width: Platform.isPad ? 400 : 320,
-
-		bottom: Platform.isPad ? 180 : 60,
-		right: Platform.isPad ? '' : 30,
+		bottom: Platform.isPad ? 80 : 60,
+		right: Platform.isPad ? 200 : 30,
 
 		zIndex: 999,
 	},
@@ -96,6 +104,39 @@ const styles = StyleSheet.create({
 		flex: 1,
 		justifyContent: 'center',
 		alignItems: 'center',
-		marginTop: 30,
+		// marginTop: 30,
+		backgroundColor: 'rgba(0,0,0,0.5)',
+	},
+	// Style for the modal with price
+	priceModalContainer: {
+		flex: 1,
+		justifyContent: 'flex-end',
+		backgroundColor: 'rgba(0,0,0,0.6)',
+		height: '100%',
+	},
+	modalCheckOutContainer: {
+		backgroundColor: 'white',
+		padding: 16,
+		height: 500,
+		borderWidth: 1,
+	},
+	restaurantName: {
+		textAlign: 'center',
+		fontWeight: '600',
+		fontSize: 18,
+		marginBottom: 10,
+	},
+	subTotalContainer: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		marginTop: 15,
+		marginHorizontal: 20,
+		paddingLeft: 10,
+	},
+	subtotalText: {
+		textAlign: 'left',
+		fontWeight: '700',
+		fontSize: 15,
+		marginBottom: 10,
 	},
 });
